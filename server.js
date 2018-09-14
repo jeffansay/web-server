c1eeb084365a18646ac37c4f099a1f805b545206
jeffansay <31795555+jeffansay@users.noreply.github.com>
2018-09-15 01:57:55 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+// variable
+const app = express();
+const port = 3000;
+const msg = `Running on port 3000`;
+
+app.set('view engine', 'hbs');
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    const log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n');
+
+    
+    next();
+});
+
+app.use((req, res) => {
+        res.render('maintainance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+        // res.send('<h1>hello express</h1>');
+       res.render('index.hbs', {
+            pageTitle: 'Index Page',
+            welcomMsg: 'Welcome to my website',
+         
+       });
+});
+
+
+app.get('/about', (req, res) => {
+        res.render('about.hbs', {
+            pageTitle: 'About Page',
+        
+        });
+});
+
+app.get('/bad', (req, res) => {
+        res.send({
+            errorMessage: 'Unable to handle such a request'
+        })
+});
+
+// app listen for local server
+app.listen(port, () => {
+    console.log(msg);
+    
+});
